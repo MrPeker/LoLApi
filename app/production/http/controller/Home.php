39a20dc43f9da14241ed088add332e714495527d
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-18 00:47:08 +0300

    Developing

--- app/production/http/controller/Home.php
+++ app/production/http/controller/Home.php
@@ -3,6 +3,8 @@ namespace App\Production\Http\Controller;
 
 use Libs\Connect\Connect;
 use duncan3dc\Laravel\Blade;
+use Libs\Input\Input;
+use System\Response;
 use System\Session;
 
 class Home
@@ -18,4 +20,21 @@ class Home
           echo Blade::render('home');
       }
   }
+
+    public function indexPost()
+    {
+        if(!Session::exists('login'))
+        {
+            $summoner =(strtolower($summoner));
+            $data = api('summoner/by-name/' . $summoner, '1.4', 'tr')->$summoner;
+            Session::set('login', true);
+            Session::set('summoner', $data->name);
+            Session::set('id', $data->id);
+            Session::set('profile_icon_id', $data->profileIconId);
+            Session::set('revision_date', $data->revisionDate);
+            Session::set('level', $data->summonerLevel);
+        }
+        Response::back();
+    }
+
 }
