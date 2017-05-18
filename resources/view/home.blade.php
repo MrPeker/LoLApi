4e620d35ec1cece624229a404019dce9517bff40
Mehmet Ali Peker <maps6134@hotmail.com>
2017-05-18 23:08:16 +0300

    Developing

--- resources/view/home.blade.php
+++ resources/view/home.blade.php
@@ -15,23 +15,36 @@
             <div class="summoner_img">
                 <img src="https://avatar.leagueoflegends.com/tr/{{ \System\Session::get('summoner') }}.png" alt="">
             </div>
-            <div class="summoner_name">{{ \System\Session::get('summoner') }}</div>
+            <div class="summoner_name">{{ \System\Session::get('summoner') }} - {{ $matches->totalGames }} Dereceli Maç</div>
         </div>
     </div>
     <div class="body">
         <div class="matches">
+            @php
+                $i = 0;
+            @endphp
             @foreach ($matches->matches as $match)
             @php
                 $champion = \Database\Databases\Lol\ChampionTable::find($match->champion);
             @endphp
+            @if($i == 0)
+                <style>
+                    body {background: #000; }
+                </style>
+                <div class="backdrop" style="background-image: url('https://lolstatic-a.akamaihd.net/game-info/1.1.9/images/champion/backdrop/bg-{{ strtolower($champion->champ_key)  }}.jpg')"></div>
+                @php
+                    $i = 1;
+                @endphp
+            @endif
             <div class="match">
 
                 <div class="match_head">
-                    <div class="match_image">
                         <img src="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/{{ trim($champion->champ_key) }}.png" alt="">
-                    </div>
                     <div class="match_title">
-                        {{ $champion->name  }}
+                        {{ $champion->name  }} - {{ $match->lane  }}
+                        @if($match->role != 'NONE')
+                            ({{ $match->role }})
+                        @endif
                     </div>
                 </div>
                 <div class="match_body">
